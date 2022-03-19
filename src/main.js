21f86795953c1839e08aabb6ca23c4efe60dfb5d
Michel Moreira <michel.moreira@academico.ifpb.edu.br>
2022-03-19 11:40:26 -0300

    Port config

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ const app = new App({
   target: document.body,
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 export default app;
